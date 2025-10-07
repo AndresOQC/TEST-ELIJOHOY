5efc19cc5a910443f5eb721213e86c40f8f037fa
AndresOQC <andresquiliche@codeplai.pe>
2025-10-07 18:59:36 -0500

    app4

--- elijohoy-fronted/dist/spa/assets/QSpace-DcyflFuO.js
+++
@@ -1,1 +0,0 @@
-import{m as a,x as s}from"./index-B0DP0xMs.js";const p=a({name:"QSpace",setup(){const e=s("div",{class:"q-space"});return()=>e}});export{p as Q};
--- elijohoy-fronted/dist/spa/assets/pinia-D_eoVW6W.js
+++
@@ -1,1 +0,0 @@
-import{b as o,c as t}from"./index-B0DP0xMs.js";const e=o(({app:a})=>{const i=t();a.use(i)});export{e as default};
