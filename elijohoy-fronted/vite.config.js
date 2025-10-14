bfceadb6e181968f1156ebdbfe536360308970d4
AndresOQC <andresquiliche@codeplai.pe>
2025-10-14 02:59:55 -0500

    dominio

--- elijohoy-fronted/vite.config.js
+++ elijohoy-fronted/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     server: {
-        allowedHosts: ['elijohoy.com', 'www.elijohoy.com'],
+        allowedHosts: ['elijohoy.com', 'www.elijohoy.com', 'localhost', '185.111.156.248'],
         host: '0.0.0.0',
         port: 9000,
     }
