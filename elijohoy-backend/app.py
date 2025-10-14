50ab15a6a6c525153c7b768a35001f178737d7a0
AndresOQC <andresquiliche@codeplai.pe>
2025-10-14 00:56:22 -0500

    dominio

--- elijohoy-backend/app.py
+++ elijohoy-backend/app.py
@@ -1,7 +1,15 @@
 from app.core.app_factory import create_app
+from flask import request, jsonify
 import os
 
 app = create_app()
+
+@app.route('/api', methods=['GET'])
+def api_root():
+    period = request.args.get('period')
+    if period:
+        return jsonify({"message": f"Periodo recibido: {period}"})
+    return jsonify({"error": "Periodo no especificado"}), 400
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
--- elijohoy-fronted/vite.config.js
+++ elijohoy-fronted/vite.config.js
@@ -1,9 +1,13 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   server: {
     allowedHosts: ['elijohoy.com', 'www.elijohoy.com'], // Agrega los dominios permitidos
     fs: {
       allow: ['..', '/root/elijohoy/elijohoy-fronted', './'] // Agrega './' para permitir el uso del archivo .env.production
-    }
+    },
+    host: '0.0.0.0', // Asegúrate de que el servidor escuche en todas las interfaces
+    port: 9000, // Puerto del frontend
   },
-  // ...resto de la configuración
-};
+  // ...otras configuraciones
+});
