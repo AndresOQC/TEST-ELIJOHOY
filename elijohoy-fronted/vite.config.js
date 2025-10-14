81173b9b987dade1be1628b75c326c1485a58fc1
AndresOQC <andresquiliche@codeplai.pe>
2025-10-13 23:35:43 -0500

    dominio

--- elijohoy-fronted/vite.config.js
+++ elijohoy-fronted/vite.config.js
@@ -1,6 +1,9 @@
 export default {
   server: {
     allowedHosts: ['elijohoy.com', 'www.elijohoy.com'], // Agrega los dominios permitidos
+    fs: {
+      allow: ['..', '/root/elijohoy/elijohoy-fronted', './'] // Agrega './' para permitir el uso del archivo .env.production
+    }
   },
   // ...resto de la configuración
 };
